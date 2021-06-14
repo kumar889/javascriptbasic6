2de0bc739382df7019d767761496b2504071f289
kumar889 <kumar.bajura@gmail.com>
2021-06-14 19:23:48 +0545

    Assignment8,commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// declared a variable, and also use prompt to take input from user
+var number = prompt('Enter a number');
+
+// this statement will process the input and give the matching output
+if (number > 0){
+    document.write('The number is +ve');
+} else {
+    document.write('It should be a number greater than 0');
+}
